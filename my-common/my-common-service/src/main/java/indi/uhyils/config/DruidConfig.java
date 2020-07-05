2d35639d7d0fa7d3fcd331894e90ac82952a15b1
Administrator <admin@example.com>
2020-07-05 09:03:59 +0800

    添加druid 连接线程池

---
+++ my-common/my-common-service/src/main/java/indi/uhyils/config/DruidConfig.java
@@ -0,0 +1,55 @@
+package indi.uhyils.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置所有项目的druid控制台
+ *
+ * @author uhyils <247452312@qq.com>
+ * @date 文件创建日期 2020年07月05日 08时45分
+ */
+@Configuration
+public class DruidConfig {
+    @Bean
+    public ServletRegistrationBean druidServlet() { // 主要实现WEB监控的配置处理
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                // 现在要进行druid监控的配置处理操作
+                new StatViewServlet(), "/druid/*");
+        // 白名单
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        // 黑名单
+        // servletRegistrationBean.addInitParameter("deny", "192.168.1.200");
+        // 用户名
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        // 密码
+        servletRegistrationBean.addInitParameter("loginPassword", "admin");
+        // 是否可以重置数据源
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        // 所有请求进行监控处理
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.css,/druid/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+}
