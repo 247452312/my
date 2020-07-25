5f9f1d48e2e271004dec20a6656cc9bf90112634
Administrator <admin@example.com>
2020-07-25 09:36:30 +0800

    取消了matlab方法.因为matlab引擎启动太慢了,准备修改为java代码

--- my-service/my-service-algorithm/src/main/java/indi/uhyils/serviceImpl/AlgorithmServiceImpl.java
+++ my-service/my-service-algorithm/src/main/java/indi/uhyils/serviceImpl/AlgorithmServiceImpl.java
@@ -6,6 +6,8 @@ import indi.uhyils.pojo.response.base.ServiceResult;
 import indi.uhyils.service.AlgorithmService;
 
 /**
+ * 算法类
+ *
  * @author uhyils <247452312@qq.com>
  * @date 文件创建日期 2020年07月04日 18时31分
  */
--- my-service/my-service-algorithm/src/main/java/indi/uhyils/util/Algorithm.java
+++ my-service/my-service-algorithm/src/main/java/indi/uhyils/util/Algorithm.java
@@ -1,11 +1,5 @@
 package indi.uhyils.util;
 
-import indi.uhyils.util.audio.main.MFCCFeatureMain;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 /**
  * 算法集合
  *
@@ -13,30 +7,5 @@ import java.io.IOException;
  * @date 文件创建日期 2020年07月19日 09时16分
  */
 public class Algorithm {
-    public static double[] getMfccVoice() throws IOException {
-        FileInputStream fis = null;
-        File file = null;
-        try {
-            Object[] objects = MatlabCellUtil.matlabCell("MatlabVideo.Video", "getVideo", "temp");
-            assert objects != null;
-            String fileName = objects[0].toString();
-            file = new File(fileName);
-            return MFCCFeatureMain.getFeature(file);
-        } catch (Exception e) {
-            LogUtil.error(Algorithm.class, e);
-        } finally {
-            if (fis != null) {
-                fis.close();
-            }
-            if (file != null && file.exists()) {
-                file.delete();
-            }
-        }
-        return null;
-    }
 
-    public static void main(String[] args) throws IOException {
-        double[] mfccVoice = getMfccVoice();
-
-    }
 }
