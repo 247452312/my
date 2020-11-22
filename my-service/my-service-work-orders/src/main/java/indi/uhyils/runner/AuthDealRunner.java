99686453dd5c08a3463590f3b956dd44a2d42ca1
Administrator <uhyils@qq.com>
2020-11-22 20:29:50 +0800

    完善处理工单节点 未完成 等待自动处理模块完成

---
+++ my-service/my-service-work-orders/src/main/java/indi/uhyils/runner/AuthDealRunner.java
@@ -0,0 +1,20 @@
+package indi.uhyils.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自动处理工单的地方
+ *
+ * @author uhyils <247452312@qq.com>
+ * @date 文件创建日期 2020年11月22日 17时44分
+ */
+@Component
+public class AuthDealRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
