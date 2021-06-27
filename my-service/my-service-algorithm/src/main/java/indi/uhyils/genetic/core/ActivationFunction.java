9228862fe550cb3cd2b070be7ed2d29b7433f181
龙龙 <shangyilong@come-future.com>
2021-06-27 13:38:26 +0800

    所有抽象接口类方法似乎已经完成,等待感冒结束,完整梳理一遍,最好是拿着纸币去规划一遍, 看看有哪部分有遗漏 上一个遗传算法写的时候对接口等不熟悉,所以才导致现在越来越乱,一个好的规划直接导致了项目最终能达到的目标

--- my-service/my-service-algorithm/src/main/java/indi/uhyils/genetic/core/ActivationFunction.java
+++ my-service/my-service-algorithm/src/main/java/indi/uhyils/genetic/core/ActivationFunction.java
@@ -8,4 +8,23 @@ package indi.uhyils.genetic.core;
  * @date 文件创建日期 2021年06月22日 08时32分
  */
 public interface ActivationFunction {
+
+
+    /**
+     * 正向计算
+     *
+     * @param result 正向计算中与权重相乘相加计算出来的值
+     * @return
+     */
+    double forward(double result);
+
+
+    /**
+     * 反向计算
+     *
+     * @param result 反向计算中此神经节点需要修正的值
+     * @return
+     */
+    double reverse(double result);
+
 }
